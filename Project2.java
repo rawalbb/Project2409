d0ab95d8b83426d7564af53614dfd6728982fdf3
rawalbb <rawalbb@vcu.edu>
2018-09-30 16:05:56 -0400

    Main file

---
+++ Project2.java
@@ -0,0 +1,29 @@
+
+public class Project2 
+{
+	
+	
+	
+	public static void main(String[] args)
+	{
+		hardActivation(.75);
+		softActivation(.75);
+		hardActivation(.5);
+		softActivation(.5);
+		
+	}
+	public void readData()
+	{
+		File values = new File("Enter data text file");
+		Scanner scanValues = new Scanner(values);
+		
+	}
+	public static void softActivation(double trainingData)
+	{
+		
+	}
+	public static void hardActivation(double trainingData)
+	{
+		
+	}
+}
