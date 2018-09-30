ff6fe75d6de165ae69a940abdacf81f15d85dc63
Vishakha Sehgal <sehgalv@vcu.edu>
2018-09-30 17:35:07 -0400

    adds data.csv, reads file in, and stores data

--- Project2.java
+++ Project2.java
@@ -1,29 +1,59 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Project2 
-{
-	
-	
-	
-	public static void main(String[] args)
-	{
-		hardActivation(.75);
-		softActivation(.75);
-		hardActivation(.5);
-		softActivation(.5);
-		
-	}
-	public void readData()
-	{
-		File values = new File("Enter data text file");
-		Scanner scanValues = new Scanner(values);
-		
-	}
-	public static void softActivation(double trainingData)
-	{
-		
-	}
-	public static void hardActivation(double trainingData)
-	{
-		
-	}
+public class Project2 {
+    public static void main(String[] args) throws IOException {
+        readData();
+        hardActivation(0.75);
+        softActivation(0.75);
+        hardActivation(0.25);
+        softActivation(0.25);
+    }
+
+    public static void readData() throws IOException {
+        ArrayList<Person> allDataPoints= new ArrayList<>();
+        File dataFile = new File("data.csv");
+        Scanner scanFile = new Scanner(dataFile);
+
+        while(scanFile.hasNextLine()) {
+            String dataPoint = scanFile.nextLine();
+            String[] data = dataPoint.split(",");
+            allDataPoints.add(new Person(Double.parseDouble(data[0]), Double.parseDouble(data[1]), Integer.parseInt(data[2].trim())));
+        }
+    }
+
+    public static void softActivation(double trainingData) {
+
+    }
+
+    public static void hardActivation(double trainingData) {
+
+    }
+}
+
+class Person {
+
+    private double height;
+    private double weight;
+    private int gender;
+
+    public Person(double height, double weight, int gender) {
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public int getGender() {
+        return this.gender;
+    }
 }
