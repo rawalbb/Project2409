d47dc42883ef46430d83476b9b66da3dacf12f04
rawalbb <rawalbb@vcu.edu>
2018-09-30 18:19:33 -0400

    net and weight change calculation

--- Project2.java
+++ Project2.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 
 public class Project2 {
     public static void main(String[] args) throws IOException {
+    	ArrayList <Person> arr = new ArrayList <Person>();
+		double a = 0.3;
         readData();
-        hardActivation(0.75);
-        softActivation(0.75);
-        hardActivation(0.25);
-        softActivation(0.25);
+        hardActivation(0.75, a, arr);
+        softActivation(0.75, a, arr);
+        hardActivation(0.25, a, arr);
+        softActivation(0.25, a, arr);
     }
 
     public static void readData() throws IOException {
@@ -24,11 +26,38 @@ public class Project2 {
         }
     }
 
-    public static void softActivation(double trainingData) {
+    public static void softActivation(double trainingData, double learning, ArrayList<Person> list) {
+    	
+		double net = 0.0;
+		double weightMultiplier = 0.0;
+		double[] weights = new double[3];
+		double[] weightChange = new double[3];
+		for(int i =0;i<((list.size())*trainingData); i++)
+		{
+			int out;
+			net= ((list.get(i).getHeight()*weights[0]) + (list.get(i).getHeight()*weights[1]) + (list.get(i).getGender()*weights[2]));
+			if (net > 0)
+			{
+				out = 1;
+			}
+			else
+			{
+				out = 0;
+			}
+			weightMultiplier = learning * (list.get(i).getGender() - out);
+			weightChange[0] = list.get(i).getHeight() * weightMultiplier;
+			weightChange[1] = list.get(i).getWeight() * weightMultiplier;
+			weightChange[2] = list.get(i).getGender() * weightMultiplier;
+			
+			for(int j=0; j<weights.length; j++)
+			{
+				weights[j] = weights[j]-weightChange[j];
+			}
+		}
 
     }
 
-    public static void hardActivation(double trainingData) {
+    public static void hardActivation(double trainingData, double learning, ArrayList<Person> list) {
 
     }
 }
